7a01f90e2f6b060fa102fe4fd151df83e5a8da12
Jean-Denis Dion <jdenisdion@gmail.com>
2014-02-06 20:57:14 -0500

    Commit de instance 1

---
+++ src/instance1/Instance1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package instance1;
+
+/**
+ *
+ * @author dd691317
+ */
+public class Instance1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hugo est cool");
+    }
+    
+}
